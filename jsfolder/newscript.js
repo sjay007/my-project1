baa2b76eeb77c8e094d517d35d069dca598725b0
sjay007 <108139201+sjay007@users.noreply.github.com>
2022-06-24 15:46:20 +0100

    Add files via upload

---
+++ jsfolder/newscript.js
@@ -0,0 +1,11 @@
+
+function subscribe(){
+
+  document.querySelector(".s-form").style.display = "block";
+  document.querySelector("#sub-btn").style.display = "none";
+}
+
+function submitted(){
+    document.querySelector(".s-form").style.display = "none";
+    document.querySelector(".result").style.display = "block";
+}
